662f389e64f8a3c8981377a275bc5ade382ff80b
Aditya R Rudra <adityaberasia@gmail.com>
2021-06-18 23:55:52 +0530

    Plant model added...

---
+++ models/plant.js
@@ -0,0 +1,63 @@
+const DB = require('../config/database');
+const { DataTypes } = require('sequelize-cockroachdb');
+
+const Plant = DB.define('plants', {
+    plant_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    description: {
+        type: DataTypes.STRING(100),
+        allowNull: true
+    },
+    water_amount: {
+        type: DataTypes.STRING(50),
+        allowNull: true
+    },
+    sunlight_needed: {
+        type: DataTypes.STRING(50),
+        allowNull: true
+    },
+    active_seasons: {
+        type: DataTypes.STRING(100),
+        allowNull: true
+    },
+    active_from: {
+        type: DataTypes.STRING(100),
+        allowNull: true
+    },
+    manure: {
+        type: DataTypes.STRING(100),
+        allowNull: true
+    },
+    diseases: {
+        type: DataTypes.STRING(100),
+        allowNull: true
+    },
+    growing_method: {
+        type: DataTypes.STRING(100),
+        allowNull: true
+    },
+    special_requirements: {
+        type: DataTypes.STRING(100),
+        allowNull: true
+    },
+    additional_info: {
+        type: DataTypes.STRING(150),
+        allowNull: true
+    },
+    author_name: {
+        type: DataTypes.STRING(60),
+        allowNull: true
+    }
+});
+
+Plant.sync({
+    force: true
+}).then((plant) => {
+    console.log('=> Plant table connected!');
+});
+
+module.exports = Plant;
