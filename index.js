0525a4c014ec72c101fac9b61c335d1e435318dd
Aditya R Rudra <adityaberasia@gmail.com>
2021-06-20 01:01:18 +0530

    Multer added..

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const morgan = require('morgan');
 const _ = require('lodash');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const {login, register} = require('./controllers/auth');
+const {login, register, authenticate} = require('./controllers/auth');
+const {insertJournal, getAllJournals, getJournalById} = require('./controllers/journal');
+const {uploadAndGetPredictions} = require('./controllers/plant');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 
 
 const app = express();
@@ -35,8 +38,33 @@ app.use((req, res, next) => {
     next();
 });
 
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, __dirname + '/uploads');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+    onFileUploadStart: function (file) {
+        console.log(file.fieldname + ' is starting ...')
+    },
+    onFileUploadData: function (file, data) {
+        console.log(data.length + ' of ' + file.fieldname + ' arrived')
+    },
+    onFileUploadComplete: function (file) {
+        console.log(file.fieldname + ' uploaded to  ' + file.path)
+    }
+
+});
+   
+let upload = multer({ storage: storage })
+
 app.post('/login', login);
 app.post('/register', register);
+app.post('/journal/create', authenticate, insertJournal);
+app.get('/journal/getAll', authenticate, getAllJournals);
+app.post('/journal/get', authenticate, getJournalById);
+app.post('/plantDetails', upload.single('myFile'), uploadAndGetPredictions);
 
 
 app.listen(3000, function() {
